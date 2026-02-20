95dd74239e8a44a160cf4d744f2bc28a6733dd1c
Onur Çakılı <onurcakili0@gmail.com>
2026-02-20 17:23:56 +0300

    Create 2_meta_script

---
+++ apiProject/2_meta_script.py
@@ -0,0 +1,26 @@
+import requests
+import pandas as pd
+
+
+# Ayarlar
+ACCESS_TOKEN = "x"  
+AD_ACCOUNT_ID = "x"  # örn: act_123456789
+
+# Çekilecek metrikler
+params = {
+    "fields": "campaign_name,impressions,clicks,spend,reach",
+    "date_preset": "last_7d",  # son 7 gün
+    "level": "campaign",
+    "access_token": ACCESS_TOKEN,
+}
+
+# API isteği
+url = f"https://graph.facebook.com/v19.0/{AD_ACCOUNT_ID}/insights"
+
+response = requests.get(url, params=params)
+
+data = response.json()
+
+df_meta_ads = pd.DataFrame(data["data"])
+
+df_meta_ads
